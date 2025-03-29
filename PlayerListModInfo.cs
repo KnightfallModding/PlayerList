142a36c3305960eb686c96fad105c58f26e5673b
Aeryle <contact.aeryle@pm.me>
2025-03-29 01:58:37 +0100

    :bookmark: release v1.0.2

--- PlayerListModInfo.cs
+++ PlayerListModInfo.cs
@@ -4,7 +4,7 @@ public static class PlayerListModInfo
 {
   public const string MOD_NAME = "PlayerList";
   public const string MOD_GUID = "com.aeryle.playerlist";
-  public const string MOD_VERSION = "1.0.1";
+  public const string MOD_VERSION = "1.0.2";
   public const string MOD_AUTHOR = "Aeryle";
   public const string MOD_LINK = "https://github.com/KnightfallModding/PlayerList";
 }
